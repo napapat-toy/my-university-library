11646e83a2c9d3e15e02ced7e42ce6fd1649b72f
toytoy08 <toy31327@gmail.com>
2025-04-04 04:43:51 +0700

    Deploy on vercel

---
+++ app/api/auth/imagekit/route.ts
@@ -0,0 +1,19 @@
+import config from "@/lib/config";
+import ImageKit from "imagekit";
+import { NextResponse } from "next/server";
+
+const {
+  env: {
+    imagekit: { publicKey, privateKey, urlEndpoint },
+  },
+} = config;
+
+const imageKit = new ImageKit({
+  publicKey,
+  privateKey,
+  urlEndpoint,
+});
+
+export async function GET() {
+  return NextResponse.json(imageKit.getAuthenticationParameters());
+}
