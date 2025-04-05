26515f0d0a976f6bd258e325f410d8cc79c6c6bd
toytoy08 <toy31327@gmail.com>
2025-04-06 04:29:23 +0700

    implement qstash workflows for user onboarding

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
@@ -10,8 +11,11 @@ const config = {
     upstash: {
       redisUrl: process.env.UPSTASH_REDIS_URL!,
       redisToken: process.env.UPSTASH_REDIS_TOKEN!,
-      qstashUrl:process.env.QSTASH_URL!,
-      qstashToken:process.env.QSTASH_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToken: process.env.QSTASH_TOKEN!,
+    },
+    resend: {
+      resendToken: process.env.RESEND_TOKEN!,
     },
   },
 };
